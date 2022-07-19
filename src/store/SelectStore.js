a764bdce28cb33b71bb38cc9ffd2d32cc9fb0537
Tong Chen <1141425427@qq.com>
2022-07-19 17:33:19 +0100

    Select Store to Control Apperence of Delivery Form

--- src/store/SelectStore.js
+++ src/store/SelectStore.js
@@ -1,8 +1,8 @@
 /*
- * @Description: 
+ * @Description: Auto Data of Selected Centre to Control Apperence of Delivery Form
  * @Author: Tong Chen
  * @Date: 2022-07-18 23:05:35
- * @LastEditTime: 2022-07-19 00:40:51
+ * @LastEditTime: 2022-07-19 17:31:57
  * @LastEditors:  
  */
 import { makeAutoObservable } from "mobx"
