65a1ebc95a282fc74e508894757ac946c2d92099
Tong Chen <1141425427@qq.com>
2022-08-17 17:57:12 +0100

    Index view

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@
  * @Description: index.js for User
  * @Author: Tong Chen
  * @Date: 2022-07-18 16:37:01
- * @LastEditTime: 2022-07-19 17:53:14
+ * @LastEditTime: 2022-08-13 16:18:00
  * @LastEditors:  
  */
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Router from './route'
+import Router from './routes/index'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
