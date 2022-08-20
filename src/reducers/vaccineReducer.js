ba950c9e7868f7872ddeb7fba7e6001c6f6a21f5
Tong Chen <1141425427@qq.com>
2022-08-20 22:46:17 +0100

    Reducer for consumption

--- src/reducers/vaccineReducer.js
+++ src/reducers/vaccineReducer.js
@@ -2,29 +2,61 @@
  * @Description: 
  * @Author: Tong Chen
  * @Date: 2022-08-13 17:11:16
- * @LastEditTime: 2022-08-17 16:27:17
+ * @LastEditTime: 2022-08-20 00:45:13
  * @LastEditors:  
  */
+import { consumeAction } from '../actions/consumeAction'
+
 const initState = () => {
-  console.log("enter initState")
   return {
     centreInfo: null
   }
 }
 
 const vaccineReducer = (state = initState(), action) => {
-  console.log("enter Reducer")
   switch (action.type) {
     case 'GET_CENTRE': {
-      console.log("enter reducer", action, state)
+      // console.log("enter reducer", action, state)
+      // set timers for consumption
+      // let timerIDs = []
+      // action.centreInfo.forEach(item => {
+      //   timerIDs.push(
+      //     setInterval(() => {
+      //       item.initVaccine -= 5
+      //       console.log(item.initVaccine)
+      //     }, 5000)
+      //   )
+      // })
+
+      return {
+        centreInfo: action.centreInfo,
+        timerID: action.timerID
+      }
+    }
+
+    case 'CLEAR_TIMER': {
+      // state.timerID.forEach(item => {
+      //   console.log("clear")
+      //   clearInterval(item)
+      // })
+      console.log("clear")
+      clearInterval(state.timerID)
+      return state
+    }
+
+    case 'CONSUME_VACCINE': {
+      state.centreInfo.forEach(item => {
+        item.initVaccine -= item.rateComsumption
+        console.log("consume", item.initVaccine)
+      })
+
       return {
         ...state,
-        centreInfo: action.centreInfo
+        centreInfo: state.centreInfo
       }
     }
 
     default: {
-      console.log("enter default")
       return state
     }
   }
