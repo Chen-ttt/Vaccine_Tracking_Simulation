54e6c46988975474be59ebf32d93fc5575dac1f5
Tong Chen <1141425427@qq.com>
2022-08-20 22:45:51 +0100

    Clear Timer & Consumption Aciton

--- src/actions/consumeAction.js
+++ src/actions/consumeAction.js
@@ -2,38 +2,70 @@
  * @Description: 
  * @Author: Tong Chen
  * @Date: 2022-08-13 16:43:08
- * @LastEditTime: 2022-08-17 16:30:07
+ * @LastEditTime: 2022-08-20 00:35:36
  * @LastEditors:  
  */
 
 import axios from "axios"
 import "../mockDatabase/centresDB"
 
-const getCentreAction = (data) => {
-  console.log("entre get Centre action")
-  return {
-    type: 'GET_CENTRE',
-    centreInfo: data
-  }
-}
+// const getCentreAction = (data) => {
+//   console.log("entre get Centre action")
+//   return {
+//     type: 'GET_CENTRE',
+//     centreInfo: data
+//   }
+// }
 
-const getCentreDB = () => {
-  console.log("entre get CentreDB")
+const initCentreDB = () => {
   return (dispatch, getState) => {
     axios.get('/centreMock', { dataType: 'json' })
-      .then((res) => {
-        console.log("Success to mock! ", res.data.centreInfo)
+      .then(res => {
+        console.log("Success to mock centre database! ")
+        return res.data.centreInfo
+      })
+      .then(data => {
+        // let timerIDs = []
+        // data.forEach(item => {
+        //   timerIDs.push(
+        //     setInterval(() => {
+        //       // item.initVaccine -= 5
+        //       console.log(item.initVaccine)
+        //       dispatch(consumeAction(item.rateComsumption))
+        //     }, 5000)
+        //   )
+        // })
+
+        let timerID = setInterval(() => {
+          dispatch(consumeAction())
+        }, 5000)
+
         dispatch({
           type: 'GET_CENTRE',
-          centreInfo: res.data.centreInfo
+          centreInfo: data,
+          timerID: timerID
         })
       })
       .catch((err) => {
-        console.log("Fail to mock database: ", err)
+        console.log("Fail to mock centre database: ", err)
       })
+  }
+}
+
+const consumeAction = (rate) => {
+  return {
+    type: 'CONSUME_VACCINE'
+  }
+}
+
+const clearAction = () => {
+  return {
+    type: 'CLEAR_TIMER'
   }
 }
 
 export {
-  getCentreDB
+  initCentreDB,
+  clearAction,
+  consumeAction
 }
