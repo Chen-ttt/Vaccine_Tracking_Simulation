f732aab7d1936a7cdfb2401e135401ecfc159dfe
Tong Chen <1141425427@qq.com>
2022-08-22 00:02:06 +0100

    Config Infor

---
+++ src/mockDatabase/config.js
@@ -0,0 +1,28 @@
+/*
+ * @Description: 
+ * @Author: Tong Chen
+ * @Date: 2022-08-21 22:31:03
+ * @LastEditTime: 2022-08-21 23:41:54
+ * @LastEditors:  
+ */
+export const centresName = [
+  "Welfare Pharmacy",
+  "Stubley Medical Centre",
+  "Page Hall Pharmacy",
+  "NHS Vaccination Centre",
+  "Seven Hills Pharmacy",
+  "Archway Pharmacy"
+]
+
+export const manufacturerName = [
+  "NISSIN Easson Factory",
+  "TOYO Enterprise",
+  "CNC-Takag Formi",
+  "Awea Matech",
+  "Sogotec UVA",
+  "Leadwell Laser",
+  "Palmary Virker",
+  "Little Giant",
+  "Spring Factory",
+  "Hendersons Relish"
+]
