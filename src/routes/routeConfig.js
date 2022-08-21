c1764db50cbff5feb0104ac20cbd3dc6d01f7ba8
Tong Chen <1141425427@qq.com>
2022-08-22 00:00:19 +0100

    Second Level Routes

--- src/routes/routeConfig.js
+++ src/routes/routeConfig.js
@@ -2,22 +2,55 @@
  * @Description: Route config
  * @Author: Tong Chen
  * @Date: 2022-08-02 00:30:29
- * @LastEditTime: 2022-08-15 20:10:27
+ * @LastEditTime: 2022-08-21 21:04:14
  * @LastEditors:  
  */
 
-import App from "../views/App"
+import App from "../views/HomePage/App"
+import Root from "../views/Details/Root"
+import HomeRoot from "../views/HomeRoot"
+import { MenuButton } from "../views/MenuButton"
+import HomeLineChart from "../components/HomeLineChart"
 // import Service from "../service/Service"
 
 export default [
   {
     path: '/',
-    element: App,
+    element: HomeRoot,
+    exact: true,
+    subRoutes: [
+      {
+        path: '/home',
+        element: App
+      },
+      {
+        path: '/centres',
+        element: MenuButton
+      },
+      {
+        path: '/service',
+        element: MenuButton
+      },
+      {
+        path: '/details/:id',
+        element: Root,
+        exact: true,
+      },
+    ]
+  },
+  {
+    path: '/test',
+    element: HomeLineChart,
     exact: true,
   },
   // {
-  //   path: '/service',
-  //   element: Service,
+  //   path: '/test1',
+  //   element: TestRoot,
   //   exact: true,
   // },
+  // {
+  //   path: '/test2',
+  //   element: PrimarySearchAppBar,
+  //   exact: true
+  // }
 ]
