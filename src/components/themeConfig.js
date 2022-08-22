ef72a0c9609f91ec86c875c9aec89b1f331a6dfe
Tong Chen <1141425427@qq.com>
2022-08-23 00:47:20 +0100

    Config of Theme

---
+++ src/components/themeConfig.js
@@ -0,0 +1,17 @@
+/*
+ * @Description: 
+ * @Author: Tong Chen
+ * @Date: 2022-08-22 22:24:46
+ * @LastEditTime: 2022-08-22 22:28:28
+ * @LastEditors:  
+ */
+
+const colorPalette = {
+  bgRed: "#E35656",
+  bgYellow: "#FFD754", //ffb74d
+  bgGreen: "#9BD128"
+}
+
+export {
+  colorPalette
+}
