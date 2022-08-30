3d660a2d8757d59b6820461c5d300b35c4e9d5d5
Tong Chen <1141425427@qq.com>
2022-08-30 22:49:54 +0100

    Add Progress Bar & Delivery Info

--- src/views/Details/Details.js
+++ src/views/Details/Details.js
@@ -2,22 +2,22 @@
  * @Description: 
  * @Author: Tong Chen
  * @Date: 2022-08-15 19:36:02
- * @LastEditTime: 2022-08-30 17:36:35
+ * @LastEditTime: 2022-08-30 22:43:50
  * @LastEditors:  
  */
 import LineChart from "../../components/LineChart"
 import { useParams } from "react-router-dom"
 import { connect } from 'react-redux'
 import { CardRoot, StyledCard } from '../../components/CardStyle'
-import { Card, Grid } from '@mui/material'
+import { Card, Grid, LinearProgress, CircularProgress, Icon } from '@mui/material'
 import { InforBox } from "./InforBox"
 import ManBox from "./ManBox"
 import DoctorBox from "./DoctorBox"
+import NewsBox from "./NewsBox"
 
 function DetailPage ({ centres, mans, doctors }) {
   const params = useParams()
   const curCentre = centres[params.id]
-  const deliveryInfo = curCentre.deliveryInfo
 
   return (
     <div>
@@ -29,33 +29,31 @@ function DetailPage ({ centres, mans, doctors }) {
       </Card>
 
       <Grid container spacing={3}>
-        <Grid item xs={8}>
+        <Grid item lg={8} md={8} sm={12} xs={12}>
           <Card sx={{ px: 3, py: 2, mb: 3 }}>
             <InforBox centre={curCentre} />
           </Card>
+
+          <Grid item xs={12}>
+            <Card sx={{ px: 3, py: 2, mb: 3 }}>
+              <ManBox mans={mans} rela={curCentre.manList} />
+            </Card>
+          </Grid>
+
+          <Grid item xs={12}>
+            <Card sx={{ px: 3, py: 2, mb: 3 }}>
+              <DoctorBox doctors={doctors} rela={curCentre.doctorList} />
+            </Card>
+          </Grid>
+
         </Grid>
 
-        <Grid item xs={4}>
+        <Grid item lg={4} md={4} sm={12} xs={12}>
           <Card sx={{ px: 3, py: 2, mb: 3 }}>
-            News
-            {deliveryInfo.length !== 0 ? deliveryInfo.map((item, index) => (
-              <div key={index}>{item.man}{item.amount}</div>
-            )) : null}
-            <div id="addBlank"></div>
+            <NewsBox deliveryInfo={curCentre.deliveryInfo} />
           </Card>
         </Grid>
 
-        <Grid item xs={8}>
-          <Card sx={{ px: 3, py: 2, mb: 3 }}>
-            <ManBox mans={mans} rela={curCentre.manList} />
-          </Card>
-        </Grid>
-
-        <Grid item xs={8}>
-          <Card sx={{ px: 3, py: 2, mb: 3 }}>
-            <DoctorBox doctors={doctors} rela={curCentre.doctorList} />
-          </Card>
-        </Grid>
       </Grid>
     </div >
   )
---
+++ src/views/Details/NewsBox.js
@@ -0,0 +1,92 @@
+/*
+ * @Description: 
+ * @Author: Tong Chen
+ * @Date: 2022-08-30 22:09:29
+ * @LastEditTime: 2022-08-30 22:41:19
+ * @LastEditors:  
+ */
+
+import React from 'react'
+import { H1, H2, H3, H4, Small } from "../../components/Typography"
+import { Card, LinearProgress, styled, Icon } from '@mui/material'
+import AddTaskIcon from '@mui/icons-material/AddTask'
+import LocalShippingOutlinedIcon from '@mui/icons-material/LocalShippingOutlined'
+
+const GreyTextBox = styled('div')(
+  ({ theme }) => ({
+    marginLeft: '1.7vw',
+    marginTop: '0.5vw',
+    // marginBottom: '30px',
+    fontSize: '12px',
+    fontWeight: '500',
+    color: 'grey',
+    [theme.breakpoints.down('sm')]: { margin: '16px' },
+  })
+)
+
+const BarBox = styled('div')(
+  ({ theme }) => ({
+    marginLeft: '1.7vw',
+    marginTop: '0.5vw',
+    [theme.breakpoints.down('sm')]: { margin: '16px' },
+  })
+)
+
+export default function NewsBox ({ deliveryInfo }) {
+  const [completed, setCompleted] = React.useState(20)
+
+  React.useEffect(() => {
+    function progress () {
+      if (completed !== 100) {
+        setCompleted((oldCompleted) => {
+          return Math.min(oldCompleted + 8, 100)
+        })
+      }
+    }
+
+    const timer = setInterval(progress, 500)
+    return () => {
+      clearInterval(timer)
+    }
+  }, [])
+
+  return (
+    <>
+      <H1>News</H1>
+      <br></br>
+      {deliveryInfo.length !== 0 ? deliveryInfo.map((item, index) => {
+        console.log("from ", item.man)
+        if (item.bool) {
+          return (
+            <div key={index}>
+              <Icon color='success'>
+                <AddTaskIcon sx={{ fontSize: 25 }}></AddTaskIcon>
+              </Icon>
+              <H3 display={"inline"} marginLeft={"9px"}>{item.amount} vaccines are deliveried!</H3>
+
+              <GreyTextBox>From {item.man}</GreyTextBox>
+              <GreyTextBox>{item.time}</GreyTextBox>
+            </div>
+          )
+        } else {
+          return (
+            <div key={index}>
+              <Icon sx={{ color: "#ffa726" }}>
+                <LocalShippingOutlinedIcon sx={{ fontSize: 25 }}></LocalShippingOutlinedIcon>
+              </Icon>
+              <H3 display={"inline"} marginLeft={"9px"}>{item.amount} vaccines are on the way...</H3>
+              {index === deliveryInfo.length - 1 ? (
+                <BarBox>
+                  <LinearProgress variant="determinate" value={completed} />
+                </BarBox>
+              ) : null}
+
+              <GreyTextBox>From {item.man}</GreyTextBox>
+              <GreyTextBox>{item.time}</GreyTextBox>
+            </div>
+          )
+        }
+      }) : null}
+    </>
+  )
+}
